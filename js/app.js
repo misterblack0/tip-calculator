f618888733b27794ea1929c24e6ae60baf22995c
misterblack0 <misterblack0@protonmail.com>
2020-07-14 18:06:22 +0300

    new functionality/new design

--- js/app.js
+++ js/app.js
@@ -1,29 +1,46 @@
-const btn = document.querySelector('#calculate');
-const billAmount = document.querySelector('#bill-amount');
-const people = document.querySelector('#people');
-const tipAmount = document.querySelector('#service-tip');
+const btn = document.querySelector("#calculate");
+const closeBtn = document.querySelector(".closebtn");
+const billAmount = document.querySelector("#bill-amount");
+const tipSlider = document.querySelector("#tipRange");
+const tipOutput = document.querySelector("#tip");
+const splitSlider = document.querySelector("#splitRange");
+const splitOutput = document.querySelector("#split");
+
+tipOutput.textContent = tipSlider.value;
+splitOutput.textContent = splitSlider.value;
+
+tipSlider.oninput = function () {
+  tipOutput.textContent = this.value + "%";
+};
+
+splitSlider.oninput = function () {
+  splitOutput.textContent = this.value;
+};
 
 function calculate() {
   let total = 0;
-  total = (billAmount.value * tipAmount.value) / people.value;
-  total = Math.round(total * 100) / 100;
-  total = total.toFixed(2);
+  let billTotal = 0;
+  let splitTotal = 0;
+  total = (billAmount.value * tipSlider.value) / (splitSlider.value * 100);
+  billTotal =
+    (billAmount.value * tipSlider.value) / 100 + Number(billAmount.value);
+  splitTotal = billTotal / splitSlider.value;
+  billTotal = Math.round(billTotal * 100) / 100;
+  billTotal = billTotal.toFixed(2);
+  splitTotal = Math.round(splitTotal * 100) / 100;
+  splitTotal = splitTotal.toFixed(2);
 
-  document.querySelector('#tip').textContent = total;
-  document.querySelector('#tip-amount').style.display = 'block';
+  document.querySelector("#bill-total").textContent = billTotal;
+  document.querySelector("#split-total").textContent = splitTotal;
 
-  if (billAmount.value === '' || tipAmount.value === '') {
-    alert('Please enter values');
-    document.querySelector('.tip-amount').style.display = 'none';
+  if (billAmount.value === "") {
+    document.querySelector(".alert").style.display = "block";
     return;
-  }
-
-  if (people === '' || people <= 1) {
-    people = 1;
-    document.querySelector('#tip-amount').style.display = 'none';
-  } else {
-    document.querySelector('#tip-amount').style.display = 'block';
   }
 }
 
-btn.addEventListener('click', calculate);
+btn.addEventListener("click", calculate);
+
+closeBtn.addEventListener("click", function () {
+  this.parentElement.style.display = "none";
+});
